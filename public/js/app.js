8e50e78def9cd0cbc1bbaa2649bd0d4d8ba334be
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-08-25 20:40:49 -0700

    dash-three

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,21 @@
+// On page load
+$(function() {
+  getFoods();
+});
+
+// Function definitions
 function getFoods() {
   $.get("/foods", function(res){ 
-    console.log(res);
+    var foods = JSON.parse(res);
+    // grab foods template
+    template = _.template($("#foods-template").html());
+    // input foods into template and append to parent
+    foodItems = foods.map(function(food) {
+      return template(food);
+    });
+    // clear content (for repeated use)
+    $("#food-ul").html("");
+    // append foods to ul
+    $("#food-ul").append(foodItems);
   });
 }
