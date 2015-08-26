99e065713b536c9b008dd1ea3ee5c225185e6bb5
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-08-25 17:52:22 -0700

    starting point

---
+++ index.js
@@ -0,0 +1,27 @@
+// REQUIREMENTS //
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+var views = path.join(process.cwd(), "views/");
+
+// CONFIG //
+// serve js & css files
+app.use("/static", express.static("public"));
+app.use("/vendor", express.static("bower_components"));
+// body parser config to accept all datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// DATA //
+var foods =[
+  {id: 0, name: "Sushiritto", yumminess: "quite"},
+  {id: 1, name: "Green Eggs & Ham", yumminess: "sure"},
+  {id: 2, name: "Crayfish", yumminess: "depending"},
+  {id: 3, name: "Foie Gras", yumminess: "omg"},
+  {id: 4, name: "Kale", yumminess: "meh"}
+];
+
+// ROUTES //
+//get to root should render index.html
+
+//start server on port 3000
